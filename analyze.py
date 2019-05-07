3987959b2bc4e69a75106176696caa5a73f547e4
Swair Shah <swairshah@gmail.com>
2019-05-07 01:28:30 -0700

    feature extraction for images

--- analyze.py
+++ analyze.py
@@ -1,8 +1,39 @@
 import io 
-from google.cloud import vision
 import sys,os
+from google.cloud import vision
+import torchvision.models as models
+import torch 
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import numpy as np
 
 GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+
+#imagenet normalize
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+scaler = transforms.Resize((224, 224))
+to_tensor = transforms.ToTensor()
+
+class ResNet18():
+    def __init__(self):
+        self.model = models.resnet18(pretrained=True)
+        self.feature_layer = self.model._modules.get('avgpool')
+        self.model.eval()
+
+    def extract_feature(self, img):
+        img = Variable(normalize(to_tensor(scaler(img))).unsqueeze(0))
+        embedding = torch.zeros(512)
+        def hook(m, i, o):
+            feature_data = o.data.reshape((512))
+            embedding.copy_(feature_data)
+        h = self.feature_layer.register_forward_hook(hook)
+        self.model(img)
+        h.remove()
+        embedding_fp16 = np.array(embedding.numpy(), dtype=np.float16)
+        return embedding_fp16
 
 def detect_text(path):
     client = vision.ImageAnnotatorClient()
@@ -22,5 +53,26 @@ def detect_text(path):
 #        print('bounds: {}'.format(','.join(vertices)))
 
 if __name__ == "__main__":
-    fname = 'data/test.jpeg'
-    detect_text(fname)
+    from PIL import Image
+    import skimage.io
+
+    model = ResNet18()
+    url = "https://firebasestorage.googleapis.com/v0/b/crowdsourcesocialposts.appspot.com/o/image-posts%2F06c1eaa0-feea-42e5-8eee-b3ab3b099831?alt=media&token=cf1b9b4f-fa1f-48e4-8d64-82476cfeec1a"
+    img = Image.fromarray(skimage.io.imread(url))
+    fnames = ['01545ee202e12f8435d4a0997f4072c370de0897.jpg',
+              '01545ee202e12f8435d4a0997f4072c370de0897_1.jpg',
+              '01545ee202e12f8435d4a0997f4072c370de0897_2.jpg',
+              '01545ee202e12f8435d4a0997f4072c370de0897_3.jpg',
+              'efe1cf7044079d2dac7f8f1294f71f9244d42f80.jpg']
+
+    imgs = [Image.open('tests/images/'+i) for i in fnames]
+    features = [model.extract_feature(img) for img in imgs]
+
+    for f in features[1:]:
+        print(np.linalg.norm(f - features[0]))
+
+    import IPython
+    IPython.embed()
+
+    #fname = 'data/test.jpeg'
+    #detect_text(fname)
