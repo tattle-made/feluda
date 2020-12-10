0c856a541e74f7b67b291cc60eb6124fe0f108f8
Kruttika Nadig <kruttika17@gmail.com>
2020-12-10 13:52:35 +0530

    helper for creating the indices

---
+++ src/api-server/indices.py
@@ -0,0 +1,143 @@
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+def check_index(es, index, index_type):
+
+    """
+    Checks if an index already exists in Elasticsearch and if not, creates it according to the mapping specified for that index type.
+    Note: This uses default shard settings.
+
+    Args:
+    es - Elasticsearch client instance
+    index - (str) Name of the index 
+    type - (str) Allowed options are "text", "image" or "video"
+
+    """
+    # The next line is only for testing index creation AND SHOULD BE REMOVED
+    # es.indices.delete(index=index, ignore=[400,404])
+
+    if es.indices.exists(index=index):
+        print("Verified that {} exists".format(index))
+    else:
+        print("{} does not exist, creating it now".format(index))
+        body = get_mapping(index_type)
+        es.indices.create(index=index, body=body)
+        print("{} created".format(index))
+        
+
+def get_mapping(index_type):
+
+    mappings = {
+        "text": '''{
+                "mappings": {
+                    "properties":{
+                        "source_id": {
+                            "type": "keyword"
+                        },
+                        "source": {
+                            "type": "keyword"
+                        },
+                        "metadata": {
+                            "type": "object"
+                        },
+                        "text": {
+                            "type": "text",
+                            "analyzer": "standard"
+                        },
+                        "lang": {
+                            "type": "keyword"
+                        },
+                        "text_vec": {
+                            "type":"dense_vector",
+                            "dims": 300
+                        },
+                        "date_added": {
+                            "type": "date"
+                        }
+                    }
+                }
+            }''',
+        "image": '''{
+                "mappings": {
+                    "properties":{
+                        "source_id": {
+                            "type": "keyword"
+                        },
+                        "source": {
+                            "type": "keyword"
+                        },
+                        "metadata": {
+                            "type": "object"
+                        },
+                        "has_text": {
+                            "type": "boolean"
+                        },
+                        "text": {
+                            "type": "text",
+                            "analyzer": "standard",
+                        },
+                        "text_vec": {
+                            "type":"dense_vector",
+                            "dims": 300
+                        },
+                        "lang": {
+                            "type": "keyword"
+                        },
+                        "img_vec": {
+                            "type":"dense_vector",
+                            "dims": 512
+                        },
+                        "date_added": {
+                            "type": "date"
+                        }
+                    }
+                }
+            }''', 
+        "video": '''{
+                "mappings": {
+                    "properties":{
+                        "source_id": {
+                            "type": "keyword"
+                        },
+                        "source": {
+                            "type": "keyword"
+                        },
+                        "metadata": {
+                            "type": "object"
+                        },
+                        "has_text": {
+                            "type": "boolean"
+                        },
+                        "text": {
+                            "type": "text",
+                            "analyzer": "standard",
+                        },
+                        "text_vec": {
+                            "type":"dense_vector",
+                            "dims": 300
+                        },
+                        "lang": {
+                            "type": "keyword"
+                        },
+                        "vid_vec": {
+                            "type":"dense_vector",
+                            "dims": 512
+                        },
+                        "date_added": {
+                            "type": "date"
+                        },
+                        "is_avg": {
+                            "type": "boolean"
+                        },
+                        "duration": {
+                            "type": "float"
+                        },
+                        "n_keyframes": {
+                            "type": "integer"
+                        }
+                    }
+                }
+            }'''
+    }
+
+    return mappings[index_type]
