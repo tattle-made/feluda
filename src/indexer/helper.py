e8e1a5d4b6b5ef1f61daa59ceaaed482e7c71215
Kruttika Nadig <kruttika17@gmail.com>
2020-12-16 16:24:06 +0530

    copied api server side helper

--- src/indexer/helper.py
+++ src/indexer/helper.py
@@ -6,7 +6,6 @@ import pymongo
 from pymongo import MongoClient
 load_dotenv()
 import wget
-# from search import ImageSearch, TextSearch, DocSearch
 from analyzer import ResNet18, detect_text, image_from_url, detect_lang, doc2vec
 import cv2
 from VideoAnalyzer import VideoAnalyzer, compress_video
@@ -18,11 +17,6 @@ import uuid
 import logging
 
 try:
-    # mongo_url = os.environ['MONGO_URL']
-    # cli = MongoClient(mongo_url)
-    # db = cli[os.environ.get("DB_NAME")]
-    # coll = db[os.environ.get("DB_COLLECTION")]
-    es_host = os.environ['ES_HOST']
     es_vid_index = os.environ['ES_VID_INDEX']
     es_img_index = os.environ['ES_IMG_INDEX']
     es_txt_index = os.environ['ES_TXT_INDEX']
@@ -30,14 +24,53 @@ try:
 except Exception:
     print(logging.traceback.format_exc())
 
-
-# imagesearch = ImageSearch()
-# docsearch = DocSearch()
-# textsearch = TextSearch()
 resnet18 = ResNet18()
 
-config = {'host': es_host}
-es = Elasticsearch([config,])
+def get_text_vec(text):
+    print("Generating document vector")
+    text_vec = doc2vec(text)
+    print("Document vector generated")
+
+    if text_vec is None:
+        text_vec = np.zeros(300).tolist()
+    
+    return text_vec
+
+def get_image_vec(file_url):
+    image_dict = image_from_url(file_url)
+    image = image_dict['image']
+    image = image.convert('RGB') #take care of png(RGBA) issue
+    print("Generating image vector")
+    image_vec = resnet18.extract_feature(image)
+    print("Image vector generated")
+    return image_vec
+
+def get_vid_vec(file_url):
+    fname = '/tmp/vid.mp4'
+    print("Downloading video from url")
+    wget.download(file_url, out=fname)
+    print("video downloaded")
+    fsize = os.path.getsize(fname)/1e6
+    print("original size: ", fsize)
+    if fsize > 10:
+        print("compressing video")
+        fname = compress_video(fname)
+        fsize = os.path.getsize(fname)/1e6
+        print("compressed video size: ", fsize)
+    if fsize > 10:
+        raise Exception("Video too large")
+    video = cv2.VideoCapture(fname)
+    vid_analyzer = VideoAnalyzer(video)
+    vid_analyzer.set_fsize(fsize)
+
+    doable, error_msg = vid_analyzer.check_constraints()
+    os.remove(fname)
+
+    if not doable:
+        print(jsonify({'failed' : 1, 'error' : error_msg}))
+        return jsonify({'failed' : 1, 'error' : error_msg})
+    else:
+        return vid_analyzer
 
 def index_data(es, data):
 
@@ -45,13 +78,8 @@ def index_data(es, data):
     doc_id = data['source_id']
     if data["media_type"] == "text":
         text = data["text"]
+        text_vec = get_text_vec(text)
         lang = detect_lang(text)
-        print("Generating document vector")
-        text_vec = doc2vec(text)
-        print("Document vector generated")
-
-        if text_vec is None:
-            text_vec = np.zeros(300).tolist()
 
         doc = {
                 "source_id" : str(doc_id),
@@ -78,17 +106,10 @@ def index_data(es, data):
         return res
             
     elif data["media_type"] == "image":
-        print("data is image")
         image_url = data["file_url"]
-        image_dict = image_from_url(image_url)
-        image = image_dict['image']
-        image = image.convert('RGB') #take care of png(RGBA) issue
-        print("Generating image vector")
-        image_vec = resnet18.extract_feature(image)
-        print("Image vector generated")
+        image_vec = get_image_vec(image_url)
         detected_text = detect_text(image_dict['image_bytes']).get('text','')
         lang = detect_lang(detected_text)
-        print(lang)
         #import ipdb; ipdb.set_trace()
         if detected_text == '' or None:
             text_vec = np.zeros(300).tolist()
@@ -128,36 +149,24 @@ def index_data(es, data):
 
 
     elif data["media_type"] == "video":
-        print("data is video")
-        fname = '/tmp/vid.mp4'
-        print("Downloading video from url")
         video_url = data["file_url"]
+        vid_analyzer = get_vid_vec(video_url)
+        
+        # Indexing average vector
+        doc = {
+            "source_id" : str(doc_id),
+            "source" : data.get("source", "tattle-admin"),
+            "metadata" : data.get("metadata", {}),
+            "vid_vec" : vid_analyzer.get_mean_feature().tolist(),
+            "is_avg" : True,
+            "duration" : vid_analyzer.duration,
+            "n_keyframes" : vid_analyzer.n_keyframes,
+            "date_added": date
+                }
 
-        wget.download(video_url, out=fname)
-        print("video downloaded")
-        # fsize in MB
-        fsize = os.path.getsize(fname)/1e6
-        print("original size: ", fsize)
-        if fsize > 10:
-            print("compressing video")
-            fname = compress_video(fname)
-            fsize = os.path.getsize(fname)/1e6
-            print("compressed video size: ", fsize)
-        if fsize > 10:
-            raise Exception("Video too large")
-        video = cv2.VideoCapture(fname)
-        # print(type(video))
-        vid_analyzer = VideoAnalyzer(video)
-        # print(vid_analyzer)
-        vid_analyzer.set_fsize(fsize)
+        res = es.index(index=es_vid_index, body=doc)
 
-        doable, error_msg = vid_analyzer.check_constraints()
-        # print(doable)
-        # print(error_msg)
-        if not doable:
-            print(jsonify({'failed' : 1, 'error' : error_msg}))
-            return jsonify({'failed' : 1, 'error' : error_msg})
-
+        # Indexing keyframe vectors
         def gendata(vid_analyzer):
             for i in range(vid_analyzer.n_keyframes):
                 yield {
@@ -171,30 +180,18 @@ def index_data(es, data):
                     "duration" : vid_analyzer.duration,
                     "n_keyframes" : vid_analyzer.n_keyframes,
                     }
-
-            yield {
-                    "_index": es_vid_index,
-                    "source_id" : str(doc_id),
-                    "source" : data.get("source", "tattle-admin"),
-                    "metadata" : data.get("metadata", {}),
-                    "vid_vec" : vid_analyzer.get_mean_feature().tolist(),
-                    "is_avg" : True,
-                    "date_added": date,
-                    "duration" : vid_analyzer.duration,
-                    "n_keyframes" : vid_analyzer.n_keyframes,
-                    }
         
-        print("Generating video vectors")
-        res = eshelpers.bulk(es, gendata(vid_analyzer)) # Returns a tuple of number of indexed docs & number of errors
+        bulk_res = eshelpers.bulk(es, gendata(vid_analyzer)) # Returns a tuple of number of indexed docs & number of errors
+
         print("Video vectors indexed")
-        os.remove(fname)
+        print("Bulk indexing result: ", bulk_res) 
         return res
 
 if __name__ == "__main__":
     data = {
         "source_id": "123",
-        "media_type": "image",
-        "file_url": "https://s3.ap-south-1.amazonaws.com/sharechat-scraper.tattle.co.in/000021a7-905b-4e0a-a48e-b58b8fc305bc.jpg",
-        "metadata": {"test": "image indexing"}
+        "media_type": "video",
+        "file_url": "https://s3.ap-south-1.amazonaws.com/sharechat-scraper.tattle.co.in/000021a7-905b-4e0a-a48e-b58b8fc305bc.mp4",
+        "metadata": {"test": "text indexing"}
     }
     index_data(es, data)
