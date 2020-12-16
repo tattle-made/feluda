c14325aa9cf781d8e88c75a8a5722b2a2786c7c9
Kruttika Nadig <kruttika17@gmail.com>
2020-12-16 17:18:40 +0530

    removed old code

--- src/api-server/send.py
+++ src/api-server/send.py
@@ -5,19 +5,5 @@ import uuid
 import sys
 from controllers.queue_controller import queue_controller
 
-# credentials = pika.PlainCredentials(environ.get(
-#     'MQ_USERNAME'), environ.get('MQ_PASSWORD'))
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(
-#         host=environ.get('MQ_HOST'), 
-#         credentials=credentials,
-#         heartbeat=600,
-#         blocked_connection_timeout=300))
-# channel = connection.channel()
-
-# channel.queue_declare(queue='tattle-search-index-queue', durable=True)
-# channel.queue_declare(queue='tattle-search-report-queue', durable=True)
-# channel.confirm_delivery()
-
 def add_job_to_queue(payload):
     queue_controller.add_data_to_index_queue(payload=payload)
