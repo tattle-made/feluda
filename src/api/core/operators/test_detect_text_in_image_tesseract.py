c5528d7848bda01e90d27fb7d1e8e2bcb68785d9
Aatman Vaidya <aatmanvaidya@gmail.com>
2023-12-27 11:39:51 +0530

    chore: adding test images and making test multilingual

--- src/api/core/operators/test_detect_text_in_image_tesseract.py
+++ src/api/core/operators/test_detect_text_in_image_tesseract.py
@@ -13,9 +13,9 @@ class Test(unittest.TestCase):
         pass
 
     def test_sample_image_from_disk(self):
-        image_path = "sample_data/text.png"
+        image_path = "sample_data/hindi-text-2.png"
         image_text = detect_text_in_image_tesseract.run(image_path)
-        expected_text = "It was the best of\ntimes, it was the worst\nof times, it was the age\nof wisdom, it was the\nage of foolishness..."
+        expected_text = "( मेरे पीछे कौन आ रहा है)"
         self.assertEqual(image_text.strip(), expected_text.strip())
 
 # if __name__ == "__main__":
