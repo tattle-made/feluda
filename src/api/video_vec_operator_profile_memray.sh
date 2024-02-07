d850c056fd15fcb07ea67834a19291d72411bb4a
Aurora <5505558+duggalsu@users.noreply.github.com>
2024-02-07 19:08:02 +0530

    Fix modular operator issues - Updated shebang in shell scripts - Updated operator dockerfiles - Updated requirements

--- src/api/video_vec_operator_profile_memray.sh
+++ src/api/video_vec_operator_profile_memray.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 python -m memray run -o vid_vec_rep_resnet.$current_time.bin video_vec_operator_profile.py
 tail -f /dev/null
--- src/api/video_vec_operator_profile_pyinstrument.sh
+++ src/api/video_vec_operator_profile_pyinstrument.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 pyinstrument -r speedscope -o speedscope_vid_vec_rep_resnet.$current_time.json video_vec_operator_profile.py
 tail -f /dev/null
