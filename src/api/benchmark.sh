846253850895de5c1c85e34ee7678afc338f3423
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-02-08 10:49:16 +0530

    chore: echo statements for benchmark file

--- src/api/benchmark.sh
+++ src/api/benchmark.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 ./video_vec_operator_time.sh
+echo "test 1 done"
 ./video_vec_operator_cprofile.sh
+echo "test 2 done"
 ./video_vec_operator_profile_memray.sh
+echo "test 3 done"
 ./video_vec_operator_profile_pyinstrument.sh
+echo "test 4 done"
 tail -f /dev/null
