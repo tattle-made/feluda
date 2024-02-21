c48b72493064a7beebcefc46b6c7384ee04eb32a
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-02-21 15:02:45 +0530

    chore: skipping unmaintained tests

--- src/tests/test_handlers.py
+++ src/tests/test_handlers.py
@@ -1,12 +1,14 @@
 import unittest
 from core.feluda import Feluda
 from endpoint import health
+from unittest.case import skip
 
 
 class TestIndexApi(unittest.TestCase):
+    @skip
     def setUp(self) -> None:
         self.feluda = Feluda("config.yml")
-
+    @skip
     def test_health(self):
         rv = self.app.get("/health")
         print(rv.status_code)
--- src/tests/test_health_api.py
+++ src/tests/test_health_api.py
@@ -1,14 +1,16 @@
 import unittest
 from core.feluda import Feluda
 from endpoint import health
+from unittest.case import skip
 
 
 class TestIndexApi(unittest.TestCase):
+    @skip
     def setUp(self) -> None:
         feluda = Feluda("config.yml")
         feluda.set_endpoints([health.HealthEndpoint])
         self.app = feluda.server.app.test_client()
-
+    @skip
     def test_health(self):
         rv = self.app.get("/health")
         print(rv.status_code)
