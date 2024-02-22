7c77664c15f9f48a0c9ba847183f37b955f2abee
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-02-22 23:13:32 +0530

    feat: md5 operator

---
+++ src/core/operators/md5.py
@@ -0,0 +1,20 @@
+
+def initialize(param):
+    global hashlib
+    import hashlib
+
+def run(media_path):
+    with open(media_path, "rb") as f:
+        file_hash = hashlib.md5()
+        # file_hash = hashlib.blake2b()
+        while chunk := f.read(4092):
+            file_hash.update(chunk)
+    
+    return file_hash.hexdigest()
+
+# if __name__ == "__main__":
+#     media_file_path = r'sample_data/sample-cat-video.mp4'
+#     initialize(param={})
+#     md5_hash = run(media_file_path)
+#     print(md5_hash)
+#     print(len(md5_hash))
