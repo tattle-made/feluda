b5dbf2d6b066d23e3939ddb52619e875f39582a9
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-02-23 14:44:58 +0530

    fix: video vec test supports media factory

--- src/core/operators/test_vid_vec_rep_resnet.py
+++ src/core/operators/test_vid_vec_rep_resnet.py
@@ -1,8 +1,7 @@
 import unittest
-import vid_vec_rep_resnet
-# from feature.index.model import VideoFactory
 from unittest.case import skip
-
+from core.operators import vid_vec_rep_resnet
+from core.models.media_factory import VideoFactory
 
 class Test(unittest.TestCase):
     @classmethod
@@ -16,29 +15,25 @@ class Test(unittest.TestCase):
         # delete config files
         pass
 
+    # @skip
     def test_sample_video_from_disk(self):
-        video = {"path": r"sample_data/sample-cat-video.mp4"}
-        result = vid_vec_rep_resnet.run(video)
+        video_path = VideoFactory.make_from_file_on_disk(r"core/operators/sample_data/sample-cat-video.mp4")
+        result = vid_vec_rep_resnet.run(video_path)
         # self.assertEqual(len(list(result)), 6)
         for vec in result:
             self.assertEqual(len(vec.get('vid_vec')), 512)
 
-    @skip
+    # @skip
     def test_unsupported_sample_video_from_disk(self):
-        video = VideoFactory.make_from_file_on_disk("sample_data/cat_vid_94mb.mp4")
+        video = VideoFactory.make_from_file_on_disk("core/operators/sample_data/video_files/video-1200sec.mp4")
         with self.assertRaises(Exception) as context:
             vid_vec_rep_resnet.run(video)
-        self.assertTrue("Video too large" in str(context.exception))
+        self.assertEqual("Video too large", str(context.exception))
 
-    @skip
+    # @skip
     def test_sample_video_from_url(self):
-        import wget
-
         video_url = "https://tattle-media.s3.amazonaws.com/test-data/tattle-search/cat_vid_2mb.mp4"
-        video_path = "/tmp/video_file.mp4"
-        wget.download(video_url, out=video_path)
-        video = VideoFactory.make_from_file_on_disk(video_path)
-
-        all_vec = vid_vec_rep_resnet.run(video)
-        for vec in all_vec:
-            self.assertEqual(len(vec), 512)
+        video_path = VideoFactory.make_from_url(video_url)
+        result = vid_vec_rep_resnet.run(video_path)
+        for vec in result:
+            self.assertEqual(len(vec.get('vid_vec')), 512)
