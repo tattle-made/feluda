9d0fa4eaf2094201a15ee18c1c8c96d4c9ef9d4c
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-03-27 23:44:08 +0530

    chore: media_type is command line arg in payload writer

--- src/worker/media/media_payload_writer.py
+++ src/worker/media/media_payload_writer.py
@@ -2,6 +2,7 @@ from core.feluda import ComponentType, Feluda
 from core.logger import Logger
 from time import sleep
 import uuid
+import sys
 
 log = Logger(__name__)
 
@@ -10,13 +11,15 @@ try:
     feluda.setup()
     media_index_queue = feluda.config.queue.parameters.queues[0]["name"]
     feluda.start_component(ComponentType.QUEUE)
+    # take media_type from command line
+    media_type = sys.argv[1] if len(sys.argv) > 1 else "video"
 
     for _ in range(1):
         unique_id = str(uuid.uuid4())
         dummy_payload = {
             "id": unique_id,
             "path": "https://raw.githubusercontent.com/tattle-made/feluda/main/src/core/operators/sample_data/sample-cat-video.mp4",
-            "media_type": "video"
+            "media_type": media_type
         }
         feluda.queue.message(media_index_queue, dummy_payload)
         sleep(0.3)
