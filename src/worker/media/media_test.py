3a87ce03a3d7d21aa7f0a60a960d28516a7799a8
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-03-28 02:06:01 +0530

    fix: setting up payload for audio

--- src/worker/media/media_test.py
+++ src/worker/media/media_test.py
@@ -1,13 +1,14 @@
 from core.models.media_factory import AudioFactory
 from core.operators import audio_vec_embedding
 
-# audio_url = "https://github.com/aatmanvaidya/audio-files/blob/main/audio30.wav?raw=true"
+audio_url = "https://github.com/aatmanvaidya/audio-files/blob/main/audio30.wav"
 # audio_url = "https://github.com/aatmanvaidya/audio-files/blob/main/mp3-sample-audio.mp3"
-audio_url = "https://github.com/aatmanvaidya/audio-files/blob/main/ogg-sample-audio.ogg"
+# audio_url = "https://github.com/aatmanvaidya/audio-files/blob/main/ogg-sample-audio.ogg"
 new_url = audio_url.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/")
-dwn = AudioFactory.make_from_url_to_wav(new_url)
-print(dwn)
-audio_vec_embedding.initialize(param=None)
-audio_emb = audio_vec_embedding.run(dwn)
-audio_emb_list = audio_emb.tolist()
-print(len(audio_emb_list))
+print(new_url)
+# dwn = AudioFactory.make_from_url_to_wav(new_url)
+# print(dwn)
+# audio_vec_embedding.initialize(param=None)
+# audio_emb = audio_vec_embedding.run(dwn)
+# audio_emb_list = audio_emb.tolist()
+# print(len(audio_emb_list))
