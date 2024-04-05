83ed5cf33ba44006a91259e9b924769b0fdee777
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-04-05 11:17:05 +0530

    refactor: s3 download to a new file

--- src/core/models/media_factory.py
+++ src/core/models/media_factory.py
@@ -6,10 +6,10 @@ import numpy as np
 from werkzeug.datastructures import FileStorage
 import wget
 from core.models.media import MediaType
+from core.models.s3_utils import AWSS3Utils
 import logging
 import os
 import tempfile
-import boto3
 from pydub import AudioSegment
 
 log = logging.getLogger(__name__)
@@ -69,27 +69,6 @@ class TextFactory:
     @staticmethod
     def make_from_file_in_memory(image_data: FileStorage):
         pass
-
-class AWSS3Utils:
-    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-    aws_region = os.getenv('AWS_REGION')
-    aws_bucket = os.getenv('AWS_BUCKET')
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=aws_region
-    )
-    s3 = session.client('s3')
-
-    @staticmethod
-    def download_file_from_s3(bucket_name, file_key, local_file_path):
-        try:
-            AWSS3Utils.s3.download_file(bucket_name, file_key, local_file_path)
-            print(f"File {file_key} downloaded successfully as {local_file_path}")
-        except Exception as e:
-            print(f"Error downloading file {file_key}: {e}")
-            raise Exception("Error Downloading file from S3")
 
 class VideoFactory:
 
