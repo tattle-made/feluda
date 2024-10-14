18a7a3b38bc2c7b3675f3af009d6939c6568548f
priyash7 <priyashshah4@gmail.com>
2024-10-14 21:00:10 +0530

    added test cases

--- src/core/operators/test_detect_lang_of_audio.py
+++ src/core/operators/test_detect_lang_of_audio.py
@@ -13,7 +13,7 @@ class Test(unittest.TestCase):
         # delete config files
         pass
 
-    def test_english_detection(self):
+    def test_english_detection_audio(self):
         audio_file_path = AudioFactory.make_from_file_on_disk(
             r"core/operators/sample_data/en_speech.wav"
         )
@@ -21,7 +21,24 @@ class Test(unittest.TestCase):
         self.assertEqual(lang["id"], "en")
         self.assertEqual(lang["language"], "english")
 
-    def test_hindi_detection(self):
+    def test_english_detection_video(self):
+        audio_file_path = AudioFactory.make_from_file_on_disk(
+            r"core/operators/sample_data/en_speech.mp4"
+        )
+        lang = detect_lang_of_audio.run(audio_file_path)
+        self.assertEqual(lang["id"], "en")
+        self.assertEqual(lang["language"], "english")
+
+
+    def test_hindi_detection_video(self):
+        audio_file_path = AudioFactory.make_from_file_on_disk(
+            r"core/operators/sample_data/hi_speech.mp4"
+        )
+        lang = detect_lang_of_audio.run(audio_file_path)
+        self.assertEqual(lang["id"], "hi")
+        self.assertEqual(lang["language"], "hindi")
+
+    def test_hindi_detection_audio(self):
         audio_file_path = AudioFactory.make_from_file_on_disk(
             r"core/operators/sample_data/hi_speech.wav"
         )
@@ -29,7 +46,8 @@ class Test(unittest.TestCase):
         self.assertEqual(lang["id"], "hi")
         self.assertEqual(lang["language"], "hindi")
 
-    def test_tamil_detection(self):
+
+    def test_tamil_detection_audio(self):
         audio_file_path = AudioFactory.make_from_file_on_disk(
             r"core/operators/sample_data/ta_speech.wav"
         )
@@ -37,9 +55,25 @@ class Test(unittest.TestCase):
         self.assertEqual(lang["id"], "ta")
         self.assertEqual(lang["language"], "tamil")
 
-    def test_telugu_detection(self):
+    def test_tamil_detection_video(self):
+        audio_file_path = AudioFactory.make_from_file_on_disk(
+            r"core/operators/sample_data/ta_speech.mp4"
+        )
+        lang = detect_lang_of_audio.run(audio_file_path)
+        self.assertEqual(lang["id"], "ta")
+        self.assertEqual(lang["language"], "tamil")
+
+    def test_telugu_detection_audio(self):
         audio_file_path = AudioFactory.make_from_file_on_disk(
             r"core/operators/sample_data/te_speech.wav"
+        )
+        lang = detect_lang_of_audio.run(audio_file_path)
+        self.assertEqual(lang["id"], "te")
+        self.assertEqual(lang["language"], "telugu")
+
+    def test_telugu_detection_video(self):
+        audio_file_path = AudioFactory.make_from_file_on_disk(
+            r"core/operators/sample_data/te_speech.mp4"
         )
         lang = detect_lang_of_audio.run(audio_file_path)
         self.assertEqual(lang["id"], "te")
