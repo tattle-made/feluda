c2b6cbef795343a9778f76a0ab5b3451e6f33bd1
Priyash Shah <89684873+plon-Susk7@users.noreply.github.com>
2024-10-17 17:22:03 +0530

    feat: added support to the tesseract operator for tamil and telugu (#411)
    * 0.8.0
    
    Automatically generated by python-semantic-release
    
    * added support for tam and tel
    
    * removed print available language
    
    * added test cases
    
    * removed comments
    
    * removed main
    
    * fixed docker file
    
    ---------
    
    Co-authored-by: Aatman Vaidya <aatmanvaidya@gmail.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- src/core/operators/test_detect_text_in_image_tesseract.py
+++ src/core/operators/test_detect_text_in_image_tesseract.py
@@ -1,5 +1,6 @@
 import unittest
 import detect_text_in_image_tesseract
+import re
 
 
 class Test(unittest.TestCase):
@@ -13,10 +14,23 @@ class Test(unittest.TestCase):
         # delete config files
         pass
 
-    def test_sample_image_from_disk(self):
+    def test_sample_image_from_disk_hindi(self):
         image_path = "sample_data/hindi-text-2.png"
         image_text = detect_text_in_image_tesseract.run(image_path)
         expected_text = "( मेरे पीछे कौन आ रहा है)"
+        self.assertEqual(image_text.strip(), expected_text.strip())
+
+    def test_sample_image_from_disk_tamil(self):
+        image_path = "sample_data/tamil-text.png"
+        image_text = detect_text_in_image_tesseract.run(image_path)
+        cleaned_image_text = re.sub(r'[\u200c\u200b]', '', image_text) # remove zero width space and zero width non-joiner \u200c and \u200b
+        expected_text = "காதல் மற்றும் போர்"
+        self.assertEqual(cleaned_image_text.strip(), expected_text.strip())
+
+    def test_sample_image_from_disk_telugu(self):
+        image_path = "sample_data/telugu-text.png"
+        image_text = detect_text_in_image_tesseract.run(image_path)
+        expected_text = "నేను భూమిని ప్రేమిస్తున్నాను"
         self.assertEqual(image_text.strip(), expected_text.strip())
 
 
