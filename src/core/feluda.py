877040d0b28c3734c444519867f5df2a4776b781
priyash7 <priyashshah4@gmail.com>
2024-10-21 12:52:22 +0530

    updated feluda class and example notebook

--- src/core/feluda.py
+++ src/core/feluda.py
@@ -1,6 +1,9 @@
 import logging
 from core import config
 from enum import Enum
+from core.models.media import MediaType
+from core.models.media_factory import VideoFactory
+from datetime import datetime
 
 log = logging.getLogger(__name__)
 
@@ -33,6 +36,53 @@ class Feluda:
             from core.server import Server
 
             self.server = Server(self.config.server)
+
+    def generate_document(self,post_id: str, representation: any):
+        # This is specific to clip operator
+        base_doc = {
+            "e_kosh_id": "",
+            "dataset": post_id,
+            "metadata": None,
+            "date_added": datetime.now().isoformat(),
+        }
+
+        def generator_doc():
+            for vector in representation:
+                base_doc["_index"] = "video"
+                base_doc["vid_vec"] = vector["vid_vec"]
+                base_doc["is_avg"] = vector["is_avg"]
+                yield base_doc
+
+        return generator_doc
+
+    def store_video(self,video_url):
+        filename = video_url.split('/')[-1]
+        video = VideoFactory.make_from_url(video_url)
+        operator = self.operators.get()["vid_vec_rep_clip"]
+        embedding = operator.run(video)
+
+        if self.store:
+            doc = self.generate_document(filename,embedding)
+            media_type = MediaType.VIDEO
+            result = self.store['es_vec'].store(media_type,doc)
+            return("result:",result)
+        else:
+            raise Exception("Store is not Configured")
+
+    def search_video(self,video_url):
+        file_name = video_url.split('/')[-1]
+        video = VideoFactory.make_from_url(video_url)
+        operator = self.operators.get()["vid_vec_rep_clip"]
+        embedding = operator.run(video)
+        average_vector = next(embedding)
+
+        if self.store:
+            result = self.store['es_vec'].find("video",average_vector.get("vid_vec"))
+            return result
+        else:
+            raise Exception("Store is not Configured")
+
+
 
     def setup(self):
         if self.operators:
