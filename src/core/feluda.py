cecaaecba80774842a6359772d2f6df814151ecb
priyash7 <priyashshah4@gmail.com>
2024-10-22 15:11:07 +0530

    moved functions to notebook

--- src/core/feluda.py
+++ src/core/feluda.py
@@ -38,53 +38,6 @@ class Feluda:
 
             self.server = Server(self.config.server)
 
-    def generate_document(self,post_id: str, representation: any):
-        # This is specific to clip operator
-        base_doc = {
-            "e_kosh_id": "",
-            "dataset": post_id,
-            "metadata": None,
-            "date_added": datetime.now().isoformat(),
-        }
-
-        def generator_doc():
-            for vector in representation:
-                base_doc["_index"] = "video"
-                base_doc["vid_vec"] = vector["vid_vec"]
-                base_doc["is_avg"] = vector["is_avg"]
-                yield base_doc
-
-        return generator_doc
-
-    def store_video(self,video_url):
-        filename = video_url.split('/')[-1]
-        video = VideoFactory.make_from_url(video_url)
-        operator = self.operators.get()[self.config.operators.parameters[0].type]
-        embedding = operator.run(video)
-
-        if self.store:
-            doc = self.generate_document(filename,embedding)
-            media_type = MediaType.VIDEO
-            result = self.store[self.config.store.entities[0].type].store(media_type,doc)
-            return("result:",result)
-        else:
-            raise Exception("Store is not Configured")
-
-    def search_video(self,video_url):
-        file_name = video_url.split('/')[-1]
-        video = VideoFactory.make_from_url(video_url)
-        operator = self.operators.get()[self.config.operators.parameters[0].type]
-        embedding = operator.run(video)
-        average_vector = next(embedding)
-
-        if self.store:
-            result = self.store[self.config.store.entities[0].type].find("video",average_vector.get("vid_vec"))
-            return result
-        else:
-            raise Exception("Store is not Configured")
-
-
-
     def setup(self):
         if self.operators:
             self.operators.setup()
