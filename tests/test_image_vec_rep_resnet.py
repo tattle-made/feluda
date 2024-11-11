e71349be2e596c6efd37b05851024d9a900d06e1
priyash7 <priyashshah4@gmail.com>
2024-11-11 16:51:58 +0530

    changes to test file

--- tests/test_image_vec_rep_resnet.py
+++ tests/test_image_vec_rep_resnet.py
@@ -15,6 +15,15 @@ class Test(unittest.TestCase):
         # Clean up resources if needed
         pass
 
+    @skip
+    def test_sample_image_from_disk(self):
+        image_path = r"core/operators/sample_data/text.png"
+        image_obj = ImageFactory.make_from_file_on_disk(image_path)
+        operator = self.feluda.operators.get()["image_vec_rep_resnet"]
+        image_vec = operator.run(image_obj)
+        print(len(image_vec))
+        self.assertEqual(len(image_vec), 512)
+
     def test_sample_image_from_url(self):
         image_obj = ImageFactory.make_from_url(
             "https://tattle-media.s3.amazonaws.com/test-data/tattle-search/text-in-image-test-hindi.png"
