9bf8e172dd24a351065838e95b363dcea3e106a6
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-04-24 02:44:03 +0530

    chore: fixing the operator

--- operators/video_hashing/test.py
+++ operators/video_hashing/test.py
@@ -1,32 +1,21 @@
 import os
-import tempfile
 import unittest
 
 from feluda.models.media_factory import VideoFactory
-from operators.video_hashing.video_hashing import hash_video
+from operators.video_hashing import video_hashing
 
 
 class TestVideoHashing(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Temporary directory for downloaded files
-        cls.temp_dir = tempfile.gettempdir()
+        video_hashing.initialise(param=None)
 
     @classmethod
     def tearDownClass(cls):
-        # Clean up any leftover files in the temp directory
-        for file in os.listdir(cls.temp_dir):
-            if file.endswith(".mp4"):
-                try:
-                    os.remove(os.path.join(cls.temp_dir, file))
-                except Exception as e:
-                    print(f"Error cleaning up file {file}: {e}")
+        pass
 
     def test_video_hashing_from_url(self):
-        # Hosted video link
         video_url = "https://github.com/tattle-made/feluda_datasets/raw/main/feluda-sample-media/sample-cat-video.mp4"
-
-        # Download the video using VideoFactory
         video_obj = VideoFactory.make_from_url(video_url)
         video_path = video_obj["path"]
 
@@ -34,20 +23,14 @@ class TestVideoHashing(unittest.TestCase):
         self.assertTrue(os.path.exists(video_path))
 
         # Generate the hash
-        video_hash = hash_video(video_path)
+        video_hash = video_hashing.run(video_path)
+        print(f"VIDEO HASSHHHHHH {len(video_hash)}")
         self.assertIsNotNone(video_hash)
         self.assertTrue(isinstance(video_hash, str))
 
-        # Ensure the hash is Base64-encoded
-        self.assertTrue(all(c in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=" for c in video_hash))
-
-        # Delete the video file after processing
-        if os.path.exists(video_path):
-            os.remove(video_path)
-
-        # Ensure the file is deleted
-        self.assertFalse(os.path.exists(video_path))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertTrue(
+            all(
+                c in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+                for c in video_hash
+            )
+        )
