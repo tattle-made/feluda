38e118f21d2d810a8993324dc02d271e0f72ca62
Deepak Thappa <108463489+denma98@users.noreply.github.com>
2025-04-24 10:22:58 +0530

    feat: add video hash tmk operator (#593)
    * feat: hash operator added
    
    * fix: added test
    
    * chore: added bin
    
    * chore: fixing the operator
    
    * chore: remove tmk bin files
    
    * chore: rename video hash files
    
    ---------
    
    Co-authored-by: denma98 <deepak21319@gmail.com>
    Co-authored-by: Aatman Vaidya <aatmanvaidya@gmail.com>

---
+++ operators/video_hash_tmk/test.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+from feluda.models.media_factory import VideoFactory
+from operators.video_hash_tmk import video_hash_tmk
+
+
+class TestVideoHashing(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        video_hash_tmk.initialise(param=None)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_video_hashing_from_url(self):
+        video_url = "https://github.com/tattle-made/feluda_datasets/raw/main/feluda-sample-media/sample-cat-video.mp4"
+        video_obj = VideoFactory.make_from_url(video_url)
+        video_path = video_obj["path"]
+
+        # Ensure the video file exists
+        self.assertTrue(os.path.exists(video_path))
+
+        # Generate the hash
+        video_hash = video_hash_tmk.run(video_path)
+        self.assertIsNotNone(video_hash)
+        self.assertTrue(isinstance(video_hash, str))
+
+        self.assertTrue(
+            all(
+                c in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+                for c in video_hash
+            )
+        )
