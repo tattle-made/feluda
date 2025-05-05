407aa72f34206b9c1650faace0bcb64921a8ed9d
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-05-05 12:30:41 +0530

    chore: remove src folder (#623)

--- src/benchmark/audiovec/audio_vec_operator_multicore.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-python -m benchmark.audiovec.audio_vec_operator_multicore > output_multicore.txt
-tail -f /dev/null
--- src/benchmark/audiovec/audio_vec_operator_time.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python -m benchmark.audiovec.audio_vec_operator_time > output_time.txt
