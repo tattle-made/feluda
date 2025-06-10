ada4b2f2d7fe63e750dbd9b4083600d06615df0e
Omkar Kabde <omkarkabde@gmail.com>
2025-06-11 03:44:07 +0530

    convert unittest to pytest

--- operators/vid_vec_rep_clip/test.py
+++ operators/vid_vec_rep_clip/test.py
@@ -1,35 +1,29 @@
-import unittest
-from unittest.case import skip
+import pytest
 
 from feluda.models.media_factory import VideoFactory
 from operators.vid_vec_rep_clip import vid_vec_rep_clip
 
 
-class Test(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # initialize operator
-        param = {}
-        vid_vec_rep_clip.initialize(param)
+@pytest.fixture(scope="module", autouse=True)
+def initialize_operator():
+    vid_vec_rep_clip.initialize()
 
-    @classmethod
-    def tearDownClass(cls):
-        # delete config files
-        pass
 
-    @skip
-    def test_sample_video_from_disk(self):
-        video_path = VideoFactory.make_from_file_on_disk(
-            r"core/operators/sample_data/sample-cat-video.mp4"
-        )
-        result = vid_vec_rep_clip.run(video_path)
-        for vec in result:
-            self.assertEqual(len(vec.get("vid_vec")), 512)
+def test_sample_video_from_url():
+    video_url = (
+        "https://tattle-media.s3.amazonaws.com/test-data/tattle-search/cat_vid_2mb.mp4"
+    )
+    file = VideoFactory.make_from_url(video_url)
+    result = vid_vec_rep_clip.run(file)
+    for vec in result:
+        assert len(vec.get("vid_vec")) == 512
 
-    # @skip
-    def test_sample_video_from_url(self):
-        video_url = "https://tattle-media.s3.amazonaws.com/test-data/tattle-search/cat_vid_2mb.mp4"
-        video_path = VideoFactory.make_from_url(video_url)
-        result = vid_vec_rep_clip.run(video_path)
-        for vec in result:
-            self.assertEqual(len(vec.get("vid_vec")), 512)
+
+@pytest.mark.skip(reason="This test requires a local video file.")
+def test_sample_video_from_disk():
+    file = VideoFactory.make_from_file_on_disk(
+        "core/operators/sample_data/sample-cat-video.mp4"
+    )
+    result = vid_vec_rep_clip.run(file)
+    for vec in result:
+        assert len(vec.get("vid_vec")) == 512
